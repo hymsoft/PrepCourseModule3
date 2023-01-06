606e3d69c0f8990566350aab66da429b1234e8da
hymsoft <hymsoft@gmail.com>
2023-01-06 11:12:19 -0300

    prog01 - Variables y tipos de datos

---
+++ Variables y tipos de datos/prog01.js
@@ -0,0 +1,25 @@
+// String
+var bootcamp = "Henry";
+console.log("bootcamp: ", bootcamp);
+
+// Numeros
+var numeroEntero = 17;
+console.log("numeroEntero: ", numeroEntero);
+var numeroNegativo = -4;
+console.log("numeroNegativo: ", numeroNegativo);
+var numeroDecimal = 11.75;
+console.log("numeroDecimal: ", numeroDecimal);
+
+// Booleanos
+var estoyEntendiendo = true;
+console.log("estoyEntendiendo: ", estoyEntendiendo);
+var estoyAburrido = false;
+console.log("estoyAburrido: ", estoyAburrido);
+
+// Undefined
+var cajon1;
+console.log("cajon1: ", cajon1);
+
+// Null
+var sinNada = null;
+// console.log("sinNada: ", sinNada);
