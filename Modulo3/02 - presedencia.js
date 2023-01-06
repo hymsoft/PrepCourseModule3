c9b04a5bf8c643e6defabdda30a29cdbbb523793
hymsoft <hymsoft@gmail.com>
2023-01-06 11:20:29 -0300

    Presedencia

---
+++ Modulo3/02 - presedencia.js
@@ -0,0 +1,17 @@
+//Suma
+console.log("Suma : ", 3 + 5);
+
+//Resta
+console.log("Resta : ", 14 - 7);
+
+//Multiplicacion
+console.log("Multiplicacion : ", 8 * 6);
+
+//Division
+console.log("Division : ", 30 / 9);
+
+//Resto
+console.log("Modulo : ", 95 % 4);
+
+//Prueba de precedencia
+console.log("Presedencia : ", 3 + 5 * 2 - 8);
